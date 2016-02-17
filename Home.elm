301f86bd78842e780a8d8a472e89d4ba54836a4d
Colin Woodbury <colingw@gmail.com>
2016-02-17 12:11:17 -0800

    Started the table

--- Home.elm
+++ Home.elm
@@ -1,8 +1,23 @@
 module Home (..) where
 
 import Html exposing (..)
+import Bootstrap.Html exposing (..)
 
 ---
 
+type Review = Review String String String String
+
+--reviews :: 
+
+theHead : Html
+theHead = thead [] [ tr [] [ th [] [text "番"]
+                           , th [] [text "書名"]
+                           , th [] [text "読み終わった日"]
+                           , th [] [text "作者"]
+                           , th [] [text "一言感想"] ] ]
+
+theTable : Html
+theTable = table [] [ theHead, tbody [] [] ]
+
 main : Html
-main = text "Hi"
+main = theTable
